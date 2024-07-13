320ea6ce62fdab08be86cdaed5d6463b606edfd1
luis1558 <lucaji1998@hotmail.com>
2024-07-12 21:44:43 -0500

    introduccion ionic-storage

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-intro',
@@ -43,13 +44,14 @@ export class IntroPage implements OnInit {
     }
   ]
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private storage: Storage) { }
 
   ngOnInit() {
   }
 
   close() {
     //console.log("estoy cerrando")
+    this.storage.set('isIntroShowed', true)
     this.router.navigateByUrl("/home")
   }
 
