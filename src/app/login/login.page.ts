60e0cda69ff3a50a96a194ed1626dcaf0add7d57
luis1558 <lucaji1998@hotmail.com>
2024-07-17 19:21:14 -0500

    tema 9 validar formularios

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,21 +8,33 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
+  validation_messages = {
+    email: [
+      { type: "required", message: "Email is required" },
+      { type: "pattern", message: "Please enter a valid email" },
+    ],
+    password: [
+      { type: "required", message: "Password is required" },
+      { type: "minlength", message: "Password must be at least 5 characters long" },
+      { type: "maxlength", message: "Password must have a maximum of 10 characters" },
+
+    ]
+  }
   constructor(private formBUilder: FormBuilder) {
     this.loginForm = this.formBUilder.group({
       email: new FormControl(
         "",
         Validators.compose([
           Validators.required,
-          Validators.email
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
         ])
       ),
       password: new FormControl(
         "",
         Validators.compose([
           Validators.required,
-          Validators.minLength(8),
-          Validators.maxLength(16),
+          Validators.minLength(5),
+          Validators.maxLength(10),
         ])
       )
     })
