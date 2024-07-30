2419f8df3254b7888d5938ed3a59d52a573707ee
luis1558 <lucaji1998@hotmail.com>
2024-07-30 10:15:31 -0500

    tema 20 reproductor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,6 +18,8 @@ export class HomePage implements OnInit {
     playing: false,
     preview_url: ''
   }
+  currentSong: any = {};
+  newTime: any;
 
   constructor(private router: Router, private musicService: MusicService, private modalController: ModalController) {}
 
@@ -55,6 +57,36 @@ export class HomePage implements OnInit {
       this.song = dataReturned.data;
     })
     modal.present();
+  }
+
+  play(){
+    this.currentSong = new Audio(this.song.preview_url);
+    this.currentSong.play();
+    this.currentSong.addEventListener("timeupdate", ()=> {
+      this.newTime = (1 / this.currentSong.duration) * this.currentSong.currentTime;
+    })
+    this.song.playing = true;
+  }
+
+  pause(){
+    this.currentSong.pause();
+    this.song.playing = false;
+  }
+
+  parseTime(time = "0:00") {
+    if (time) {
+      const partTime = parseInt(time.toString().split(".")[0], 10);
+      let minutes = Math.floor(partTime/60).toString();
+      if (minutes.length == 1) {
+        minutes = "0" + minutes;
+      }
+      let seconds = (partTime % 60).toString();
+      if (seconds.length == 1) {
+        seconds = "0" + seconds;
+      }
+      return minutes + ":" + seconds;
+    }
+    return null
   }
 
 }
